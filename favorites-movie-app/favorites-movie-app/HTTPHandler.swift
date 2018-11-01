0203518726f8ccf723d8b9062b07f563a2ca5052
Tommy <itommy.moran@gmail.com>
2018-10-31 23:51:40 -0400

    HTTPHandler.swift base code added

---
+++ favorites-movie-app/favorites-movie-app/HTTPHandler.swift
@@ -0,0 +1,37 @@
+//
+//  HTTPHandler.swift
+//  favorites-movie-app
+//
+//  Created by thomas moran on 10/31/18.
+//  Copyright © 2018 thomas moran. All rights reserved.
+//
+
+import Foundation
+
+class HTTPHandler {
+    static func getJson(urlString: String, completionHandler: @escaping (Data?) -> (Void)){
+        let urlString = urlString.addingPercentEncoding(withAllowedCharacters: . urlQueryAllowed)
+        let url =  URL(string: urlString!)
+        
+        print("URL being used is \(url!)")
+        
+        print("URL being used is \(url!)")
+        let session = URLSession.shared
+        let task = session.dataTask(with: url!) { data, response, error in
+            if let data = data {
+                let httpResponse = response as! HTTPURLResponse
+                let statusCode = httpResponse.statusCode
+                print("request completed with code: \(statusCode)")
+                if (statusCode ==200) {
+                    print("return to completion handler with the data")
+                    completionHandler(data as Data)
+                }
+            } else if let error = error {
+                print("***There was an error making the HTTP request***")
+                print(error.localizedDescription)
+                completionHandler(nil)
+            }
+        }
+        task.resume()
+    }
+}
